7047c5fbb4a544f8aba48e1b0815aa65e4359e13
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-12-08 22:48:16 +0100

    Add slider to brands section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 'use strict';
 
 // MENU-BAR small screen dropdown
@@ -70,6 +71,8 @@ console.log('hello');
 >>>>>>> d6246c9... Fix css and prevent default to not refresh page when click
 =======
 >>>>>>> 05ec64b... WIP
+=======
+>>>>>>> 54bdfbf... Add slider to brands section
 // PRODUCT SECTION RATING STAR MECHANISM -- WDP181103-19
 
 const productsSection = document.querySelector(".section--products");
@@ -222,6 +225,8 @@ $(function () {
 =======
 });
 =======
+=======
+>>>>>>> Add slider to brands section
 // PRODUCT SECTION RATING STAR MECHANISM -- WDP181103-19
 
 const productsSection = document.querySelector(".section--products");
@@ -327,6 +332,138 @@ function unsetStarsClass(limit, elem, className) {
         controls: false,
         navContainer: '.feedback-indicators',
     });
+<<<<<<< HEAD
 })();  
 >>>>>>> WIP
+<<<<<<< HEAD
 >>>>>>> 05ec64b... WIP
+=======
+=======
+=======
+// PRODUCT SECTION RATING STAR MECHANISM -- WDP181103-19
+
+const productsSection = document.querySelector(".section--products");
+const starBoxes = document.querySelectorAll(".stars");
+
+productsSection.addEventListener("mouseover", function(e) {
+    if (event.target.classList.contains('star')) {
+        let arr = Object.values(event.relatedTarget.children);
+        let index = arr.indexOf(event.target);
+        deactivateStars();
+        setStarsClass(index, event.relatedTarget, "active");
+    }
+    else if (event.target.classList.contains('stars')) {
+        deselectStars();
+    }   
+});
+
+productsSection.addEventListener("click", function(e) {
+    if (event.target.classList.contains('star')) {
+        let arr = Object.values(event.target.parentElement.children);
+        let index = arr.indexOf(event.target);
+        event.preventDefault();
+        deactivateStars();
+        unsetStarsClass(4, event.target.parentElement, "permanent");
+        setStarsClass(index, event.target.parentElement,"permanent");
+    }
+});
+
+// deselecting stars
+function deselectStars() {
+    if (! event.target.children[0].classList.contains("permanent")){
+        setStarsClass(1, event.target, "full");
+    }
+    unsetStarsClass (4, event.target, "active");
+}
+// remove stars showing avg value
+function deactivateStars() {
+    unsetStarsClass (4, event.target.parentElement, "full");
+}
+
+// helper functions
+
+function setStarsClass(limit, elem, className) {
+        for (i=0; i<=limit; i++) {
+            elem.children[i].classList.add(className);
+        }
+}
+
+function unsetStarsClass(limit, elem, className) {
+        for (i=0; i<=limit; i++) {
+            elem.children[i].classList.remove(className);
+        }
+}
+
+
+// MENU-BAR small screen dropdown
+(function(){ 
+    var showBtn = document.querySelector('.category-list-show');
+    var dropdownList = document.getElementById('menu-dropdown');
+
+    showBtn.addEventListener('click', function() {
+        dropdownList.classList.toggle('drop');
+        showBtn.classList.toggle('active');
+    });
+})();  
+
+//TINY-SLIDER - NEW FURNITURE
+
+(function(){
+    var indicators = document.querySelectorAll('.carousel-indicator');
+    indicators.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+        });
+    });
+
+    var productsSlider = tns({
+        container: '.products-slider',
+        items: 1,
+        slideBy: 1,
+        mouseDrag: true,
+        controls: false,
+        navContainer: '.products-carousel-indicators',
+        responsive: {
+            768: {
+                items: 2,
+                slideBy: 2,
+            },
+            992: {
+                items: 3,
+                slideBy: 3,
+            },
+            1200: {
+                items: 4,
+                slideBy: 4,
+            }
+          }
+    });
+
+    var feedbackSlider = tns({
+        container: '.feedback-wrapper',
+        autoWidth: true,
+        mouseDrag: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        controls: false,
+        navContainer: '.feedback-indicators',
+    });
+
+    const brandsSlider = tns({
+        container: '.brands-slider',
+        loop: true,
+        items: 6,
+        slideBy: 'page',
+        nav: false,    
+        autoplay: true,
+        speed: 400,
+        autoplayButtonOutput: false,
+        mouseDrag: true,
+        lazyload: true,
+        controlsContainer: ".brands-carousel-controls",
+      });
+
+>>>>>>> a6f9e02... Add slider to brands section
+})();  
+>>>>>>> Add slider to brands section
+>>>>>>> 54bdfbf... Add slider to brands section
